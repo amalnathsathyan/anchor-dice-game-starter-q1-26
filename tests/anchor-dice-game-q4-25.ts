1a5eb67af97184e5ef5601a10c653ff1037cfea5
amalnathsathyan <amalnathsathyan@gmail.com>
2026-02-14 20:25:48 +0530

    adding tests

--- tests/anchor-dice-game-q4-25.ts
+++ tests/anchor-dice-game-q4-25.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorDiceGameQ425 } from "../target/types/anchor_dice_game_q4_25";
 import { randomBytes } from "crypto";
+import { text } from "stream/consumers";
 
 describe("anchor-dice-game-q4-25", () => {
   // Configure the client to use the local cluster.
@@ -15,6 +16,7 @@ describe("anchor-dice-game-q4-25", () => {
   let player = anchor.web3.Keypair.generate();
   let [vault] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("vault"),house.publicKey.toBuffer()],program.programId);
   let seed = new anchor.BN(randomBytes(16));
+  let [bet, betBump] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("bet"), vault.toBuffer(), seed.toBuffer("le",16)], program.programId)
 
   it("Airdrop", async () => {
     await Promise.all([house,player].map(async (k)=> {
@@ -52,5 +54,49 @@ describe("anchor-dice-game-q4-25", () => {
     .signers([player])
     .rpc()
     console.log("Bet Succesful", betTx);
+  })
+
+  it("resolves a bet", async () => {
+    const betAccount = (await connection.getAccountInfo(bet,"confirmed"));
+    let sig_ix = anchor.web3.Ed25519Program.createInstructionWithPrivateKey({
+      privateKey:house.secretKey,
+      message: betAccount.data.subarray(8)
+    });
+
+    const resolveIx = await program.methods
+    .resolveBet(Buffer.from(sig_ix.data.buffer.slice(16+32,16+32+64)))
+    .accountsStrict({
+      house: house.publicKey,
+      player: player.publicKey,
+      vault,
+      bet,
+      instructionSysvar: anchor.web3.SYSVAR_INSTRUCTIONS_PUBKEY,
+      systemProgram: program.programId
+    })
+    .signers([house])
+    .instruction();
+
+    const tx = new anchor.web3.Transaction().add(sig_ix).add(resolveIx);
+
+    try {
+      const txReciept = await anchor.web3.sendAndConfirmTransaction(connection,tx,[house]);
+      console.log("txReciept:" ,txReciept.toString())
+    } catch (error) {
+      console.log("Error Resolving Bet", error)
+    }
+  })
+
+  it("refunds a bet", async () => {
+    const refundTx = await program.methods.refundBet().accountsStrict({
+      player:player.publicKey,
+      house:house.publicKey,
+      vault:vault,
+      bet: bet,
+      systemProgram: anchor.web3.SystemProgram.programId
+    })
+    .signers([player])
+    .rpc()
+
+    console.log("Refund Successful", refundTx)
   })
 });
